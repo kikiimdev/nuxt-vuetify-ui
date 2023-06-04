bc2063b02ca6ade9f85d8da474175de85f8f0bd3
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2023-06-05 05:37:18 +0800

    feat: add preview file

--- components/K/Input/File.vue
+++ components/K/Input/File.vue
@@ -1,17 +1,26 @@
 <template>
-  <KInputBase v-bind="$attrs">
-    <VFileInput
-      v-bind="{ ...inputStyle, ...$attrs }"
-      v-model="selectedFile"
-      label=""
-    />
-  </KInputBase>
+  <KBlock>
+    <KInputBase v-bind="$attrs">
+      <VFileInput
+        @change="handleOnChange"
+        @click:clear="previewData = ''"
+        v-bind="{ ...inputStyle, ...$attrs }"
+        v-model="selectedFile"
+        label=""
+      />
+    </KInputBase>
+
+    <div v-if="showPreview" id="preview">
+      <img v-if="previewData" :src="previewData" />
+    </div>
+  </KBlock>
 </template>
 
 <script setup lang="ts">
   const props = defineProps({
     modelValue: String,
     multiple: Boolean,
+    showPreview: Boolean,
   })
   const emit = defineEmits(["update:modelValue"])
 
@@ -31,6 +40,28 @@
   })
 
   const inputStyle = useInputStyle()
+
+  const previewData = ref("")
+  const handleOnChange = (e: any) => {
+    previewData.value = ""
+    const file = e.target.files[0]
+    const isImageFile = file.type.includes("image")
+
+    if (isImageFile) {
+      previewData.value = URL.createObjectURL(file)
+    }
+  }
 </script>
 
-<style></style>
+<style>
+  #preview {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #preview img {
+    max-width: 100%;
+    max-height: 500px;
+  }
+</style>
