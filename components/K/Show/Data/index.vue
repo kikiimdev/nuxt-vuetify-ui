f66ec84c0ab767df9b137754f9ac8c4ad3a3d3fa
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2023-08-01 14:19:25 +0800

    fix: default items

--- components/K/Show/Data/index.vue
+++ components/K/Show/Data/index.vue
@@ -8,25 +8,10 @@
     <slot name="prepend-table" />
     <NuxtErrorBoundary>
       <slot name="table" :items="items" :total="total">
-        <VDataTableServer
-          :headers="headers"
-          :loading="pending"
-          :items="items"
-          :items-length="total"
-          :items-per-page="options.itemsPerPage"
-          :class="tableClass"
-          @update:options="updateOptions($event)"
-        >
-          <template
-            v-for="header of headers"
-            #[`item.${header.key}`]="{ item }"
-          >
-            <slot
-              :key="`item.${header.key}`"
-              :name="`item.${header.key}`"
-              :item="item"
-              :header="header"
-            >
+        <VDataTableServer :headers="headers" :loading="pending" :items="items" :items-length="total"
+          :items-per-page="options.itemsPerPage" :class="tableClass" @update:options="updateOptions($event)">
+          <template v-for="header of headers" #[`item.${header.key}`]="{ item }">
+            <slot :key="`item.${header.key}`" :name="`item.${header.key}`" :item="item" :header="header">
               <div :class="header.class">
                 {{ getValue(item, header) }}
               </div>
@@ -47,127 +32,127 @@
 </template>
 
 <script setup lang="ts">
-  import { VDataTableServer } from "vuetify/lib/labs/components"
+import { VDataTableServer } from "vuetify/lib/labs/components"
 
-  const props = defineProps({
-    title: String,
-    url: String,
-    headers: Array as PropType<any[]>,
-    dataKey: {
-      type: String,
-      default: "results",
+const props = defineProps({
+  title: String,
+  url: String,
+  headers: Array as PropType<any[]>,
+  dataKey: {
+    type: String,
+    default: "results",
+  },
+  totalKey: {
+    type: String,
+    default: "total",
+  },
+  query: {
+    type: Object as PropType<any>,
+    default: () => {
+      return {}
     },
-    totalKey: {
-      type: String,
-      default: "total",
-    },
-    query: {
-      type: Object as PropType<any>,
-      default: () => {
-        return {}
-      },
-    },
-    tableClass: {
-      type: String,
-    },
-  })
+  },
+  tableClass: {
+    type: String,
+  },
+})
 
-  const options = ref({
-    page: 1,
-    itemsPerPage: 10,
-    sortBy: [],
-    groupBy: [],
-  })
+const options = ref({
+  page: 1,
+  itemsPerPage: 10,
+  sortBy: [],
+  groupBy: [],
+})
 
-  const updateCount = ref(0)
-  const updateOptions = (newOptions: any) => {
-    if (updateCount.value > 0) {
-      options.value = newOptions
-    }
-    updateCount.value++
+const updateCount = ref(0)
+const updateOptions = (newOptions: any) => {
+  if (updateCount.value > 0) {
+    options.value = newOptions
+  }
+  updateCount.value++
+}
+
+const urlQuery = computed(() => {
+  const usedQuery = {
+    page: options.value.page,
+    itemsPerPage: options.value.itemsPerPage,
+    ...props.query,
   }
 
-  const urlQuery = computed(() => {
-    const usedQuery = {
-      page: options.value.page,
-      itemsPerPage: options.value.itemsPerPage,
-      ...props.query,
-    }
-
-    if (options.value.sortBy?.length) {
-      const _sortBy: any = options.value.sortBy[0]
-      usedQuery.sortBy = `${_sortBy.key}:${_sortBy.order}`
-    }
-
-    return usedQuery
-  })
-
-  const { data, pending, refresh } = await useFetch<any>(props.url!, {
-    query: urlQuery,
-    watch: [urlQuery],
-    async onRequest({ request, options }) {
-      // Log request
-      // console.log("[fetch request]", request, options)
-    },
-    async onRequestError({ request, options, error }) {
-      // Log error
-      console.log("[fetch request error]", request, error)
-    },
-    async onResponse({ request, response, options }) {
-      // Log response
-      // console.log("[fetch response]", request, response.status, response.body)
-    },
-    async onResponseError({ request, response, options }) {
-      // Log error
-      // console.log(
-      //   "[fetch response error]",
-      //   request
-      //   // response.status
-      //   // response.body
-      // )
-      // console.error({ data: response._data })
-      throw createError(response._data)
-    },
-  })
-
-  const items = computed(() => {
-    const selectedData = props.dataKey ? data.value[props.dataKey] : data.value
-    if (!selectedData) {
-      return []
-    }
-
-    return selectedData
-  })
-
-  const total = computed(() =>
-    data.value && props.totalKey ? data.value[props.totalKey] : 0
-  )
-
-  const getObjectValue = (obj: any, path: string): any => {
-    if (obj === null || obj === undefined) return null
-    if (!path) return obj
-    const properties: any[] = path.split(".")
-
-    return getObjectValue(obj[properties.shift()], properties.join("."))
+  if (options.value.sortBy?.length) {
+    const _sortBy: any = options.value.sortBy[0]
+    usedQuery.sortBy = `${_sortBy.key}:${_sortBy.order}`
   }
 
-  const getValue = (item: any, header: any) => {
-    let value = getObjectValue(toRaw(item.raw), header.key)
-    if (header.transform) {
-      value = header.transform(value)
-    }
+  return usedQuery
+})
 
-    return value
+const { data, pending, refresh } = await useFetch<any>(props.url!, {
+  query: urlQuery,
+  watch: [urlQuery],
+  async onRequest({ request, options }) {
+    // Log request
+    // console.log("[fetch request]", request, options)
+  },
+  async onRequestError({ request, options, error }) {
+    // Log error
+    console.log("[fetch request error]", request, error)
+  },
+  async onResponse({ request, response, options }) {
+    // Log response
+    // console.log("[fetch response]", request, response.status, response.body)
+  },
+  async onResponseError({ request, response, options }) {
+    // Log error
+    // console.log(
+    //   "[fetch response error]",
+    //   request
+    //   // response.status
+    //   // response.body
+    // )
+    // console.error({ data: response._data })
+    throw createError(response._data)
+  },
+})
+
+const items = computed(() => {
+  const selectedData = (props.dataKey && data.value) ? data.value[props.dataKey] : data.value
+  if (!selectedData) {
+    return []
   }
 
-  const recoverFromError = async (error: any) => {
-    // await navigateTo('/');
-    error.value = null
+  return selectedData
+})
+
+const total = computed(() =>
+  data.value && props.totalKey ? data.value[props.totalKey] : 0
+)
+
+const getObjectValue = (obj: any, path: string): any => {
+  if (obj === null || obj === undefined) return null
+  if (!path) return obj
+  const properties: any[] = path.split(".")
+
+  return getObjectValue(obj[properties.shift()], properties.join("."))
+}
+
+const getValue = (item: any, header: any) => {
+  let value = getObjectValue(toRaw(item.raw), header.key)
+  if (header.transform) {
+    value = header.transform(value)
   }
 
-  defineExpose({
-    refresh,
-  })
+  return value
+}
+
+const recoverFromError = async (error: any) => {
+  // await navigateTo('/');
+  error.value = null
+}
+
+defineExpose({
+  refresh,
+})
 </script>
 
 <style></style>
