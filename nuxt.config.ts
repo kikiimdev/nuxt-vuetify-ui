c7d9c64104ac1c451195383ef7904c5534ecbee9
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2023-08-01 14:19:19 +0800

    build: update vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,41 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { resolve } from "path"
-
-const aliases = {
-  components: "src/components",
-  plugins: "src/plugins",
-  // vuetify: "vuetify",
-}
-
-const resolvedAliases = Object.fromEntries(
-  Object.entries(aliases).map(([key, value]) => [
-    key,
-    resolve(__dirname, value),
-  ])
-)
-
 export default defineNuxtConfig({
   css: [
     "@mdi/font/css/materialdesignicons.min.css",
     "vuetify/lib/styles/main.sass",
   ],
   build: {
-    transpile: ["vuetify", "chart.js"],
+    transpile: ["chart.js", "vuetify"],
   },
   vite: {
     define: {
       "process.env.DEBUG": false,
-    },
-    // build: {
-    //   rollupOptions: {
-    //     external: ["vuetify"],
-    //   },
-    // },
-    resolve: {
-      alias: {
-        ...resolvedAliases,
-      },
     },
   },
 })
